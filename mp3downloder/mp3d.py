dbc3d8331954cf93a7f7442bdc48a40c8ed1cd72
Hassan Haghayeghi <hertkov@gmail.com>
2013-05-02 01:42:52 +0430

    improve input method and add comments

--- mp3downloder/mp3d.py
+++ mp3downloder/mp3d.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------#
 
 		# realy ugly script i know
-		# but it works like a cake!
+		# but works like a cake!
 
 		# all links are from http://mp3skull.com/
 		# this is illegal website so maybe it's blocked in your country
@@ -12,15 +12,23 @@
 		# 	export http_proxy	
 		# to end of it then
 		# 	$ source .bashrc
-		# good luck
+		
+		# Usage:
+		#		$ python mp3d.py <<<<< input method
+		#		$ python mp3d.py 'led zeppelin' 'inflames' 'floods pantera'
+		#
 
 # --------------------------------------------------------------------#
 
 from BeautifulSoup import BeautifulSoup 
 import urllib2
 import os
+import sys
 
 # --------------------------------------------------------------------#
+
+def searchlink(a):
+	return 'http://mp3skull.com/mp3/'+a+'.html' # create the search link
 
 def searchforlink(soup):
 	div = soup.body.find('div', attrs={'style' : 'float:left;'})
@@ -35,7 +43,7 @@ def searchforlink(soup):
 		download = download_div.find('a')
 		print download.get('href')
 
-def check(soup):
+def check(soup): # check if whatever you search for is exist
 	try:
 		div = soup.body.find('div', attrs={'style' : 'float:left;'})
 		content = div.find('div', attrs={'id' : 'content'})
@@ -43,27 +51,34 @@ def check(soup):
 	except (NameError, AttributeError):
 		return False
 
+def search(a):
+	try:
+		page = urllib2.urlopen(a) # open html
+		soup = BeautifulSoup(page) # BeautifulSoup type
+
+		# ----------------------------------------------------------------#
+
+		if check(soup): # is there what i want?
+			searchforlink(soup)
+		else:
+			print 'Not Found'
+
+		# ----------------------------------------------------------------#
+
+	except Exception, e: # if an error happend
+		print str(e)	# print it and die
+
 # --------------------------------------------------------------------#
 
 os.system('clear')
-
-try:
-	search = raw_input('search: ').replace(' ', '_')
-	search = 'http://mp3skull.com/mp3/'+search+'.html'
-
-	page = urllib2.urlopen(search)
-	soup = BeautifulSoup(page)
-
-	# ----------------------------------------------------------------#
-
-	if check(soup):
-		searchforlink(soup)
-	else:
-		print 'Not Found'
-
-	# ----------------------------------------------------------------#
-
-except Exception, e:
-	print str(e)
+if len(sys.argv) > 1: # how you want to search?
+	for i, x in enumerate(sys.argv):
+		if i != 0:
+			print '\n'+ x + ' results: \n'
+			srch = x.replace(' ', '_')
+			search(searchlink(srch))
+else:
+	srch = raw_input('search: ').replace(' ', '_')
+	search(searchlink(srch))
 
 # --------------------------------------------------------------------#
